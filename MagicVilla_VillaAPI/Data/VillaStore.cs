b05facc8eab1dd806e668ad9ec1cb4c1accc75a3
ne4eHbka58 <artem03082006@mail.ru>
2025-05-12 18:42:00 +0300

    Добавлены Delete, Put и Patch запросы в VillaAPIController

--- MagicVilla_VillaAPI/Data/VillaStore.cs
+++ MagicVilla_VillaAPI/Data/VillaStore.cs
@@ -6,9 +6,9 @@ namespace MagicVilla_VillaAPI.Data
     {
         public static List<VillaDTO> villaList = new List<VillaDTO>
         {
-            new VillaDTO { Id = 1, Name = "Pull View" },
-            new VillaDTO { Id = 2, Name = "Beach View" },
-            new VillaDTO { Id = 3, Name = "Ocean Villa"}
+            new VillaDTO { Id = 1, Name = "Pull View", Occupancy = 5, Sqft = 100 },
+            new VillaDTO { Id = 2, Name = "Beach View", Occupancy = 3, Sqft = 75 },
+            new VillaDTO { Id = 3, Name = "Ocean Villa", Occupancy = 4, Sqft = 80 }
         };
     }
 }
--- MagicVilla_VillaAPI/Models/Villa.cs
+++ MagicVilla_VillaAPI/Models/Villa.cs
@@ -4,5 +4,8 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public int Occupancy { get; set; }
+        public int Sqft { get; set; }
+        public DateTime CreatedDate { get; set; }
     }
 }
