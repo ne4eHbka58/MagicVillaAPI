b3cfb63d9f75d78549d06eef05f12ddaace482a5
ne4eHbka58 <artem03082006@mail.ru>
2025-05-12 12:16:16 +0300

    Обновлён VillaStore

--- MagicVilla_VillaAPI/Data/VillaStore.cs
+++ MagicVilla_VillaAPI/Data/VillaStore.cs
@@ -7,7 +7,8 @@ namespace MagicVilla_VillaAPI.Data
         public static List<VillaDTO> villaList = new List<VillaDTO>
         {
             new VillaDTO { Id = 1, Name = "Pull View" },
-            new VillaDTO { Id = 2, Name = "Beach View" }
+            new VillaDTO { Id = 2, Name = "Beach View" },
+            new VillaDTO { Id = 3, Name = "Ocean Villa"}
         };
     }
 }
