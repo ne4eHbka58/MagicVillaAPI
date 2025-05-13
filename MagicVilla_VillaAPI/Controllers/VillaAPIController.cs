079041b4ce756929b296dc34c964b28a16d9e1fe
ne4eHbka58 <artem03082006@mail.ru>
2025-05-13 12:43:20 +0300

    Добавлены логи для контроллера (VillaAPIController)

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -11,9 +11,17 @@ namespace MagicVilla_VillaAPI.Controllers
     [ApiController]
     public class VillaAPIController : ControllerBase
     {
+        private readonly ILogger<VillaAPIController> _logger;
+        public VillaAPIController(ILogger<VillaAPIController> logger)
+        {
+            _logger = logger;
+        }
+
+
         [HttpGet]
         public ActionResult<IEnumerable<VillaDTO>> GetVillas()
         {
+            _logger.LogInformation("Getting all villas");
             return Ok(VillaStore.villaList); 
         }
 
@@ -25,16 +33,19 @@ namespace MagicVilla_VillaAPI.Controllers
         {
             if (id == 0 )
             {
-                return BadRequest();
+                _logger.LogError("Get Villa Error (Id = 0) with Id = " + id);
+                return BadRequest("");
             }
 
             var villa = VillaStore.villaList.FirstOrDefault(v => v.Id == id);
 
             if (villa == null)
             {
-                return NotFound(); 
+                _logger.LogError("Get Villa Error (Not found) with Id = " + id);
+                return NotFound(""); 
             }
 
+            _logger.LogInformation("Getting Villa with Id = " + id);
             return Ok(villa);
         }
 
@@ -51,23 +62,27 @@ namespace MagicVilla_VillaAPI.Controllers
 
             if(VillaStore.villaList.FirstOrDefault(u => u.Name.ToLower() == villaDTO.Name.ToLower()) != null)
             {
+                _logger.LogError("Create Villa Error (Villa has already existing name)");
                 ModelState.AddModelError("CustomError", "Villa already Exists!");
                 return BadRequest(ModelState);
             }
 
             if (villaDTO == null)
-            {
+            { 
+                _logger.LogError("Create Villa Error (Empty villa)");
                 return BadRequest(villaDTO);
             }
 
             if (villaDTO.Id > 0) 
             {
+                _logger.LogError("Create Villa Error with Id (Id != 0) with Id = " + villaDTO.Id);
                 return StatusCode(StatusCodes.Status500InternalServerError);
             }
 
             villaDTO.Id = VillaStore.villaList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
             VillaStore.villaList.Add(villaDTO);
 
+            _logger.LogInformation("Successfully creating villa with Id " + villaDTO.Id);
             return CreatedAtRoute("GetVilla", new { id = villaDTO.Id }, villaDTO);
         }
 
@@ -79,6 +94,7 @@ namespace MagicVilla_VillaAPI.Controllers
         {
             if (id == 0 )
             {
+                _logger.LogError("Delete Villa Error (Id = 0) with Id = " + id);
                 return BadRequest();
             }
 
@@ -86,30 +102,41 @@ namespace MagicVilla_VillaAPI.Controllers
 
             if (villa == null)
             {
+                _logger.LogError("Delete Villa Error (Villa not found) with Id = " + id);
                 return NotFound();
             }
 
             VillaStore.villaList.Remove(villa);
 
+            _logger.LogInformation("Successfully deleting villa with Id = " + id);
             return NoContent();
         }
 
         [HttpPut("{id:int}", Name = "UpdateVilla")]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult<VillaDTO> UpdateVilla(int id, [FromBody]VillaDTO villaDTO)
         {
             if (villaDTO == null || id != villaDTO.Id) 
             {
+                _logger.LogError("Update Villa Error (New villa is empty or Id != new Id) with Id = " + id);
                 return BadRequest();
             }
 
             var villa = VillaStore.villaList.FirstOrDefault(v => v.Id == id);
 
+            if (villa == null)
+            {
+                _logger.LogError("Update Villa Error (Villa not found) with Id = " + id);
+                return NotFound();
+            }
+
             villa.Name = villaDTO.Name;
             villa.Occupancy = villaDTO.Occupancy;
             villa.Sqft = villaDTO.Sqft;
 
+            _logger.LogInformation("Successfully updating villa with Id = " + villaDTO.Id);
             return CreatedAtRoute("GetVilla", new { id = id }, villa);
         }
 
@@ -120,6 +147,7 @@ namespace MagicVilla_VillaAPI.Controllers
         {
             if (patchDTO == null || id == 0)
             {
+                _logger.LogError("Update Partial Villa Error (New villa is empty or Id == 0) with Id = " + id);
                 return BadRequest();
             }
 
@@ -127,6 +155,7 @@ namespace MagicVilla_VillaAPI.Controllers
 
             if (villa == null)
             {
+                _logger.LogError("Update Partial Villa Error (New villa is empty) with Id = " + id);
                 return NotFound();
             }
 
@@ -134,9 +163,11 @@ namespace MagicVilla_VillaAPI.Controllers
 
             if (!ModelState.IsValid)
             {
+                _logger.LogError("Update Partial Villa Error (New villa is not vaild) with Id = " + id);
                 return BadRequest(ModelState);
             }
 
+            _logger.LogInformation("Successfully partially updating villa with Id = " + id);
             return CreatedAtRoute("GetVilla", new { id = id }, villa);
         }
     }
